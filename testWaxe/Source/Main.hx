7c939c1a6f778558f561a6675e636b41afb7d4a2
Thomas BAUDON <baudon.thomas@gmail.com>
2014-10-05 18:02:52 +0200

    yep, ran radengine inside wxwidget.

--- testWaxe/Source/Main.hx
+++ testWaxe/Source/Main.hx
@@ -1,8 +1,13 @@
 package;
+import fr.radstar.radengine.RadGame;
 import haxe.Timer;
+import openfl.display.ManagedStage;
 import openfl.display.Shape;
 import openfl.display.Sprite;
+import openfl.display.StageAlign;
+import openfl.display.StageScaleMode;
 import openfl.Lib;
+import wx.App;
 import wx.BoxSizer;
 import wx.Button;
 import wx.Frame;
@@ -18,43 +23,44 @@ import wx.Sizer;
 
 
 class Main {
-	
-	var mFrame : Frame;
-	var mOpenfl : OpenFLStage;
-	var stage:openfl.display.ManagedStage;
-	
-	public function new () {
+
+    var frame:Frame;
+	var mStage:ManagedStage;
+     
+    public function new()
+    {
+        // Make an app window
+        frame = Frame.create(null, "WaxeApp" , null , { width:400 , height:400 } );
+         
+        // Make a panel
+        var waxePanel:Panel = Panel.create( frame );
+        var button:Button = Button.create( waxePanel, null, 'A Button');
+         
+        // Make an NME stage
+        var openflStage:OpenFLStage = OpenFLStage.create( waxePanel );
+        openflStage.stage.align = StageAlign.TOP_LEFT;
+        openflStage.stage.scaleMode = StageScaleMode.NO_SCALE;
+        
+		mStage = openflStage.stage;
 		
-		mFrame = ApplicationMain.frame;
+        // Auto position the panel and NMEStage with a sizer
+        var sizer:Sizer = BoxSizer.create(true);
+        waxePanel.set_sizer(sizer);
+        sizer.add( button , 1 , Sizer.EXPAND | Sizer.BORDER_ALL , 5 );
+        sizer.add( openflStage , 3 , Sizer.EXPAND | Sizer.BORDER_ALL , 5);
+         
+        // Show your window
+        App.setTopWindow( frame );
+        frame.shown = true;
 		
-		var panel : Panel = Panel.create(mFrame);
-		var button = Button.create(panel, 0, "Button");
-		
-		var mOpenfl = OpenFLStage.create(panel);
-		
-		var sizer : Sizer = BoxSizer.create(true);
-		panel.set_sizer(sizer);
-		sizer.add(button, 1, Sizer.EXPAND | Sizer.BORDER_ALL, 5);
-		sizer.add(mOpenfl, 3, Sizer.EXPAND | Sizer.BORDER_ALL, 5);
-		
-		stage = mOpenfl.stage;
-		
-		
-		
-		Timer.delay(aa, 1000);
-		
-		
-		
-	}
-	
-	function aa() {
-		
-		var a = new Shape();
-		a.graphics.beginFill(0xFF0000);
-		a.graphics.drawCircle(0, 0, 20);
-		
-		stage.addChild(a);
-		
-		trace(stage.numChildren);
-	}
+		var engine = new RadGame();
+		mStage.addChild(engine);
+    }
+ 
+     
+    // Entry
+    public static function main()
+    {
+        App.boot( function(){ new Main(); } );
+    }
 }
